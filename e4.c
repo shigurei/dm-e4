a31cb20c560feb20610ba073e56a189c434c5a48
shigurei <69288928+shigurei@users.noreply.github.com>
2020-08-06 14:34:46 +0900

    Victory message added

--- e4.c
+++ e4.c
@@ -3,28 +3,28 @@
 
 int main()
 {
-  int num, heads = 0, tails = 0;
-  
-  printf("Tossing a coin...");
-  for(int i = 0; i < 3; i++)
-  {
-    num = rand();
-    if(num % 2 == 0)
-    {
-      printf("Round %d: Heads\n",i);
-      heads++;
-    }else{
-      printf("Round %d: Tails\n",i);
-      tails++;
-    }   
-  }
-  printf("Heads: %d, Tails: %d\n",heads,tails);
-  if(heads > tails)
-  {
-    printf("You won.\n");
-  }else{
-    printf("You lost.\n"); 
-  }
-  
-  return 0;
+	int num, heads = 0, tails = 0;
+
+	printf("Tossing a coin...");
+	for(int i = 0; i < 3; i++)
+	{
+		num = rand();
+		if(num % 2 == 0)
+		{
+			printf("Round %d: Heads\n",i);
+			heads++;
+		}else{
+			printf("Round %d: Tails\n",i);
+			tails++;
+		}   
+	}
+	printf("Heads: %d, Tails: %d\n",heads,tails);
+	if(heads > tails)
+	{
+		printf("You won.\n");
+	}else{
+		printf("You lost.\n"); 
+	}
+
+	return 0;
 }
