caa503ac4a13401627a0fa1348924445776abe07
shigurei <69288928+shigurei@users.noreply.github.com>
2020-08-06 14:35:54 +0900

    User name added

--- e4.c
+++ e4.c
@@ -3,26 +3,26 @@
 
 int main()
 {
-  int num, heads = 0, tails = 0;
-  char arr[8];
-  printf("Who are you?/n");
-  scanf("%s",arr);
-  printf("Hello, %s!\n",arr);
+	int num, heads = 0, tails = 0;
+	char arr[8];
+	printf("Who are you?/n");
+	scanf("%s",arr);
+	printf("Hello, %s!\n",arr);
 
-  printf("Tossing a coin...");
-  for(int i = 0; i < 3; i++)
-  {
-    num = rand();
-    if(num % 2 == 0)
-    {
-      printf("Round %d: Heads\n",i);
-      heads++;
-    }else{
-      printf("Round %d: Tails\n",i);
-      tails++;
-    }
-  }
-  printf("Heads: %d, Tails: %d\n",heads,tails);
-  
-  return 0;
+	printf("Tossing a coin...");
+	for(int i = 0; i < 3; i++)
+	{
+		num = rand();
+		if(num % 2 == 0)
+		{
+			printf("Round %d: Heads\n",i);
+			heads++;
+		}else{
+			printf("Round %d: Tails\n",i);
+			tails++;
+		}
+	}
+	printf("Heads: %d, Tails: %d\n",heads,tails);
+
+	return 0;
 }
