c5511b4611d09cc58733a053c547c56a99c7cc51
shigurei <69288928+shigurei@users.noreply.github.com>
2020-08-06 14:01:51 +0900

    HoT: Initial revision

---
+++ e4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int num, heads = 0, tails = 0;
+
+printf("Tossing a coin...");
+  for(int i = 0; i < 3; i++)
+  {
+    num = rand();
+    if(num % 2 == 0)
+    {
+      printf("Round %d: Heads\n",i);
+      heads++;
+    }else{
+      printf("Round %d: Tails\n",i);
+      tails++;
+    }
+  }
+  printf("Heads: %d, Tails: %d\n",heads,tails);
+  
+  return 0;
+}
